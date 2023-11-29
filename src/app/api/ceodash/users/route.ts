893b2a2d9473d7eb472b97fec0e72bf8087c5f4a
Hunor <mogyorosi.hunor@gmail.com>
2023-11-29 12:16:32 +0100

    update

--- src/app/api/ceodash/users/route.ts
+++ src/app/api/ceodash/users/route.ts
@@ -1,4 +1,5 @@
 import { query } from "@/lib/db";
+import { getValidSubdomain } from "@/lib/url";
 import { User } from "@/types/typings";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -7,8 +8,13 @@ export async function GET(
 	{ params }: { params: { system: string } }
 ) {
 	try {
+		const subdomain = getValidSubdomain(req.headers.get("host"));
 		const queryString = `SELECT * FROM users`;
-		const res = (await query(queryString, [])) as Array<User>;
+		const res = (await query(
+			"ceodash_" + subdomain,
+			queryString,
+			[]
+		)) as Array<User>;
 
 		return new NextResponse(JSON.stringify(res), {
 			status: 200,
